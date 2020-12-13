0e81e5bd4c6e637e5b436ffe1b2b357c4df103cb
joaopd <40632469+joaopd@users.noreply.github.com>
2020-12-13 12:13:13 -0300

    Add project files.

---
+++ EfCore.Legado/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EfCore.Legado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
